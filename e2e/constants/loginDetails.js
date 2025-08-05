44ea2deb4fb9e43cc42158c8566046a9195142b1
Alex <111196124+alexd0793@users.noreply.github.com>
2025-08-05 19:01:19 +0300

    Delete e2e directory

--- e2e/constants/loginDetails.js
+++
@@ -1,7 +0,0 @@
-// constants/loginDetails.js
-const loginDetails = {
-  LOGIN_EMAIL: "alexandru.dancesc+ERIC_WRIGHT_CIVIL_ENGINEERING_LIMITED@amdaris.com",
-  LOGIN_PASSWORD: "!",
-};
-
-module.exports = loginDetails;
