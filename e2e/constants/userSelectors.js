5af9c63042e21582c8ac4da2cbed8c4fcd52c572
Alex <111196124+alexd0793@users.noreply.github.com>
2025-08-05 19:14:56 +0300

    Delete e2e directory

--- e2e/constants/userSelectors.js
+++
@@ -1,9 +0,0 @@
-class userSelectors {
-    constructor(page) {
-      this.page = page;
-      this.userProfileButton = this.page.locator('button[aria-label="Open user profile"]');
-      this.logoutButton = this.page.locator('button:has-text("Log out")');
-    }
-  }
-  
-  module.exports = userSelectors;
--- e2e/steps/hooks.js
+++
@@ -1,9 +0,0 @@
-const { Before, After } = require('@cucumber/cucumber');
-
-Before(async function () {
-  await this.launchBrowser();
-});
-
-After(async function () {
-  await this.closeBrowser();
-});
