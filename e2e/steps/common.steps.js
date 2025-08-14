84a76a4d1be34ec45399b88f627fd71a8d008271
Alex <111196124+alexd0793@users.noreply.github.com>
2025-08-14 10:39:17 +0300

    Delete e2e directory

--- e2e/steps/common.steps.js
+++
@@ -1,9 +0,0 @@
-const { Given } = require('@cucumber/cucumber');
-const LoginPage = require('../pages/LoginPage');
-
-Given('a customer is authenticated', async function () {
-  this.page = await this.browser.newPage();
-  this.loginPage = new LoginPage(this.page);
-  await this.loginPage.gotoLoginPage();
-  await this.loginPage.login();
-});
